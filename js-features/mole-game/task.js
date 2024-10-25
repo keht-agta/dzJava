ce272d88db5c9a7661d9ada143d84d7d02805b6a
Anton Pradedov <keht-agta@yandex.ru>
2024-10-25 08:33:36 +0800

    DZ3 update

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -16,8 +16,18 @@
       // Проверяем, содержит ли кликнутая лунка класс 'hole_has-mole'
       if (this.classList.contains('hole_has-mole')) {
         winCount++; // Если это активная лунка, увеличиваем счет побед
+        if(winCount >9) { 
+          alert ("Вы выйграли");
+          winCount = 0;
+          loseCount = 0;
+          }
       } else {
         loseCount++; // Иначе увеличиваем счет промахов
+        if(loseCount > 4) { 
+          alert ("Вы пройграли");
+          winCount = 0;
+          loseCount = 0;
+        }
       }
       updateScore(); // Обновляем статистику на экране
     };
